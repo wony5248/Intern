4a29ada13141d9de3fb1b4943edbff37fcb526d4
qjawls0501 <wony5248@gmail.com>
2020-02-21 10:45:28 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Greetings from "./Greetings";
+import Counter from "./Counter";
+import MyForm from "./MyForm";
+import "./App.css";
+import ReducerSample from "./ReducerSample";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const onClick = (name: string) => {
+    console.log(name);
+  };
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+  return <ReducerSample />;
+};
 
 export default App;
---
+++ src/ReducerCounter.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useReducer } from "react";
+
+type Action = { type: "INCREASE" } | { type: "DECREASE" };
+const reducer = (state: number, action: Action): number => {
+  switch (action.type) {
+    case "INCREASE":
+      return state + 1;
+    case "DECREASE":
+      return state - 1;
+    default:
+      throw new Error("Unhandled Error");
+  }
+};
+const Counter = () => {
+  const [count, dispatch] = useReducer(reducer, 0);
+  const onIncrease = () => dispatch({ type: "INCREASE" });
+  const onDecrease = () => dispatch({ type: "DECREASE" });
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={onIncrease}>+1</button>
+        <button onClick={onDecrease}>-1</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
